fd36f57ed0075aabf64cb1f50f5d5b0ca3890e79
Nate Barlow <nathanbarlow23@gmail.com>
2020-03-19 20:33:46 -0600

    finish getting player ids up to 2005

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 # from warnings import warn
+import numpy as np
 from sportsreference.ncaab.roster import Player
 
-from player_ids import id_2000
+from player_ids import *
 
-for player_id in id_2000:
-    print(f"trying player id: {player_id}")
-    player = Player(player_id)
+years = [id_2000, id_2001, id_2002, id_2003, id_2004, id_2005]
+for year in years:
+    for [player_id, is_all_star] in year:
+        player = Player(player_id)
+
+# conference, position, height, weight, offensive win shares, devensive win shares, minutes/games, fg, fga, fgp, threes, 3pa, 3pp, twos, 2pa, 2pp, ft, fta, tfp, ORB, DRB, rb, asst, steal, block, tos, fouls, points, true shooting percent, effect. field goal percent, attempt rates,
