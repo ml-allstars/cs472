ccd0f42ca8ce6a7f02daa18c6a6ddc301187399f
Nate Barlow <nathanbarlow23@gmail.com>
2020-03-19 18:45:28 -0600

    setup env

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-from warnings import warn
+# from warnings import warn
 from sportsreference.ncaab.roster import Player
 
 from player_ids import id_2000
 
 for player_id in id_2000:
+    print(f"trying player id: {player_id}")
     player = Player(player_id)
-    if player is None:
-        warn(f"Player {player_id} is NONE")
-    else:
-        print(player.__dict__)
-        # write attributes to csv
